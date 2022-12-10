543977f9deee70208116e070a4b25c08023ec6cf
Spythere <spythere@protonmail.com>
2022-12-10 09:31:10 +0100

    changes

--- pages/index.js
+++ pages/index.js
@@ -1,40 +1,53 @@
-import { useState } from "react";
-import { useRouter } from "next/router";
-import Image from "next/image";
-import { Flex, Box, Text, Icon } from "@chakra-ui/react";
-import { BsFilter } from "react-icons/bs";
-import { PieChart, Pie, Tooltip, Legend } from "recharts";
-import SearchFilters from "../components/SearchFilters";
-import { baseUrl, fetchApi } from "../utils/fetchApi";
-import noresult from "../assets/images/noresult.svg";
-import Products from "../components/Products";
+import { useEffect, useState } from 'react';
+import { useRouter } from 'next/router';
+import Image from 'next/image';
+import { Flex, Box, Text, Icon } from '@chakra-ui/react';
+import { BsFilter } from 'react-icons/bs';
+import { PieChart, Pie, Tooltip, Legend } from 'recharts';
+import SearchFilters from '../components/SearchFilters';
+import { baseUrl, fetchApi } from '../utils/fetchApi';
+import noresult from '../assets/images/noresult.svg';
+import Products from '../components/Products';
 
 const Search = ({ properties, categories }) => {
-  const [searchFilters, setSearchFilters] = useState(false);
   const router = useRouter();
+  const [searchFilters, setSearchFilters] = useState(false);
+
+  const [currentPath, setCurrentPath] = useState('');
+
+  useEffect(() => {
+    setCurrentPath(router.asPath);
+
+    return () => {};
+  }, [router.asPath]);
+
   //descructure
   const { rating, reviewCount, credibility } = { properties };
   const { category } = router.query;
   const { sort } = router.query; // get the sort parameter from the query string
 
+  const maxCred = properties.reduce((acc, prop) => {
+    if (prop.credibility > acc) acc = prop.credibility;
+
+    return acc;
+  }, 1);
+
+  properties = properties.map((prop) => ({
+    ...prop,
+    credibility: parseFloat(((prop.credibility / maxCred) * 100).toFixed(2)),
+  }));
+
   // sort the items array using the sortBy parameter
   properties.sort((a, b) => {
-    // console.log(sort, "??");
-    if (sort === "price-des") {
-      // sort by price in descending order
-      return b.price - a.price;
-    } else if (sort === "price-asc") {
-      // sort by price in ascending order
-      return a.price - b.price;
-    } else {
-      return 0;
-    }
+    return sort == 'price-des'
+      ? b.price - a.price
+      : sort == 'price-asc'
+      ? a.price - b.price
+      : b.credibility - a.credibility;
   });
 
   // Sort the categories by credibility percentage in descending order
-  const sortedCategories = categories.sort(
-    (a, b) => b.credibility - a.credibility
-  );
+  const sortedCategories = categories.sort((a, b) => b.credibility - a.credibility);
 
   // Get the top 5 categories
   const topCategories = sortedCategories.slice(0, 5);
@@ -42,15 +55,13 @@ const Search = ({ properties, categories }) => {
   // Prepare the data for the pie chart
   const pieData = topCategories.map((category) => {
     const credibilityPercentage = category.credibility;
-    const percentage = (credibilityPercentage / 100) * 100; // 47.295116234360016%
+    const percentage = parseFloat(((credibilityPercentage / 100) * 100).toFixed(2)); // 47.295116234360016%
 
     return {
       name: category.category_name,
       value: percentage,
     };
   });
-
-  console.log(pieData);
 
   return (
     <Box>
@@ -69,28 +80,26 @@ const Search = ({ properties, categories }) => {
         <Text>Search Products by filters</Text>
         <Icon paddingLeft="2" w="7" as={BsFilter} />
       </Flex>
-      {searchFilters && (
-        <SearchFilters
-          categories={categories}
-          sortedCategories={sortedCategories}
-          topCategories={topCategories}
-        />
+      {(searchFilters || currentPath == '/') && (
+        <SearchFilters categories={categories} sortedCategories={sortedCategories} topCategories={topCategories} />
       )}
       <Text fontSize="2xl" p="4" fontWeight="bold">
-        Category: {router.query.category}
+        {currentPath == '/' ? `Choose category: ` : `Category: ${router.query.category}`}
       </Text>
-      <Flex p="4" justifyContent="center">
-        {searchFilters && (
+
+      {(searchFilters || currentPath == '/') && (
+        <Flex p="4" justifyContent="center" flexDirection="column" alignItems="center">
+          <Text fontSize="2xl">Most valuable categories right now</Text>
           <PieChart width={400} height={400}>
-            <Pie dataKey="value" data={pieData} fill="#63b3ed" />
+            <Pie dataKey="value" data={pieData} label fill="#63b3ed" />
             <Tooltip />
             <Legend />
           </PieChart>
-        )}
-      </Flex>
+        </Flex>
+      )}
       <Flex flexWrap="wrap">
-        {properties.map((properties, index) => (
-          <Products key={index} products={properties} />
+        {properties.map((product, index) => (
+          <Products key={index} products={product} />
         ))}
       </Flex>
     </Box>
@@ -98,12 +107,10 @@ const Search = ({ properties, categories }) => {
 };
 
 export async function getServerSideProps({ query }) {
-  const category = query.category || "1";
-  const sort = query.sort || "price";
+  const category = query.category || '1';
+  const sort = query.sort || 'price';
 
-  const searchProduct = await fetchApi(
-    `${baseUrl}/api/products?category=${category}&sort=${sort}`
-  );
+  const searchProduct = await fetchApi(`${baseUrl}/api/products?category=${category}&sort=${sort}`);
 
   const topCategories = await fetchApi(`${baseUrl}/api/categories`);
 
@@ -116,3 +123,4 @@ export async function getServerSideProps({ query }) {
 }
 
 export default Search;
+
